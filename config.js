36f814bee06bfe2d7bf9e981fc8fed12d05277f1
fineOwO <91377161+gutboizsitinh@users.noreply.github.com>
2023-02-04 18:18:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: ` Chị trl câu hỏi của em nhé =)))))`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'acp trong game đó nha chj ?',
+    desc: 'Chj không trả lời hoặc thoát ra, tức là chj phải acp lại e đó nha :> ',
+    btnYes: 'Uh ok 💖💖💖',
+    btnNo: 'Không ! ',
+    question:'Nói cho em biết lý do chị acp lại em đi =))) 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gửii ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    reply: '1234567890',
+    mess: 'Em biết mà, Em cũng thích chj lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho em nha. 💬💬',
     btnAccept: 'Ok lun 💖',
     messLink: 'https://m.me/QuangGoodBoy'
 }
